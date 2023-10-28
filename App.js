00d6ae7c25511484b62e3de1d44b9758fa74aa4d
Felipe Urriago <durriago82@gmail.com>
2023-10-28 05:25:19 -0500

    proyecto 4

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 
+require ('dotenv').config();
+
 const express = require('express');
 
 const fs = require('fs');
@@ -9,10 +11,18 @@ const listViewRouter = require('./list-view-router');
 
 const { validarMetodoHttp } = require('./middleware');
 
+const jwt = require('jsonwebtoken');
+
+const LLAVE_SECRETA = process.env.LLAVE_SECRETA;
+
+
 
 
 const app = express();
 
+
+
+const PORT = process.env.PORT
 
 
 app.use(express.json()); 
@@ -48,6 +58,103 @@ app.get('/', (req, res) => {
 
 
 
+
+
+//base de datos usuarios 
+
+const baseDeDatosUsuario ={
+
+  userName:"user321",
+  password:"pass321",
+  rol: "manager"
+
+
+}
+
+
+
+//middleware de autenticacion
+
+function middAutenticacion (req, res, next) {
+
+  const token = req.headers.authorization;
+
+  if(!token) {
+
+    res.status(401).send("debes ingresar un token")
+
+  }
+
+  try {
+
+    const tokenDesencriptado = jwt.verify(token, LLAVE_SECRETA)
+
+    req.data = tokenDesencriptado;
+
+    next();
+
+    
+  } catch (error) {
+
+    res.status(401).send("no autorizado: el token debe ser valido")
+    
+  }
+
+
+}
+
+
+
+//ruta POST login
+
+app.post('/login', (req, res) => {
+
+  const usuario = req.body.user
+
+  const contrasena = req.body.password
+
+
+  if(usuario === baseDeDatosUsuario.userName && contrasena === baseDeDatosUsuario.password) {
+
+    const payload = {
+
+      rol: baseDeDatosUsuario.rol
+
+    }
+
+    const token = jwt.sign(payload, LLAVE_SECRETA);
+
+    res.status(200).send({msg: "bienvenido", token });
+
+
+
+  } else {
+
+    res.status(401).send("contracela o usuario incorrecto")
+
+
+
+  }
+
+
+
+});
+
+
+
+app.get('/rutaProtegida', middAutenticacion, (req, res) => {
+
+  res.status(200).send("esta es una lista de contabilidad")
+
+
+
+});
+
+
+
+
+
+
 //direccionador filtrar tareas
 
 app.use('/filtro', listViewRouter);
@@ -62,9 +169,11 @@ app.use('/editar', listEditRouter);
 
 
 
-app.listen(3000, () => {
 
-  console.log("servidor correindo"); 
+
+app.listen(PORT, () => {
+
+  console.log(`servidor correindo ${PORT}`); 
 
 });
 
